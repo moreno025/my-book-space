fc616e31edc38e0800406b2891b90f8ec73b4dc4
moreno025 <morenopedrejon@gmail.com>
2025-12-16 13:45:46 +0100

    use font

--- mobile/hooks/useFonts.ts
+++ mobile/hooks/useFonts.ts
@@ -4,6 +4,9 @@ export function useAppFonts() {
     const [fontsLoaded] = useFonts({
         'PlaywrightNorge-Regular': require('../assets/fonts/PlaywriteNO-Regular.ttf'),
         'PlaywrightNorge-Light': require('../assets/fonts/PlaywriteNO-Light.ttf'),
+        'Nunito-Regular': require('../assets/fonts/Nunito-Regular.ttf'),
+        'Nunito-Bold': require('../assets/fonts/Nunito-Bold.ttf'),
+        'Nunito-SemiBold': require('../assets/fonts/Nunito-SemiBold.ttf'),
     });
 
     return fontsLoaded;
