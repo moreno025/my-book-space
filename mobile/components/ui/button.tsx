26e14e41f564b14dec246d62081f9e85d706c09b
moreno025 <morenopedrejon@gmail.com>
2025-12-17 14:41:52 +0100

    new button design

--- mobile/components/ui/button.tsx
+++ mobile/components/ui/button.tsx
@@ -5,11 +5,13 @@ interface ButtonProps {
     title: string;
     onPress: () => void;
     style?: StyleProp<ViewStyle>;
+    disabled?: boolean;
 }
 
-export const Button = ({ title, onPress, style }: ButtonProps) => (
+export const Button = ({ title, onPress, style, disabled }: ButtonProps) => (
     <TouchableOpacity
         onPress={onPress}
+        disabled={disabled}
         style={[
             {
                 backgroundColor: "#da6c12ff",
@@ -17,6 +19,7 @@ export const Button = ({ title, onPress, style }: ButtonProps) => (
                 borderRadius: 8,
                 alignItems: "center",
                 marginTop: 16,
+                opacity: disabled ? 0.5 : 1,
             },
             style,
         ]}
